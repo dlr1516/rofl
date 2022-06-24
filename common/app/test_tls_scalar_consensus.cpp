5f1cf35ce2252a3c95ed0fb1652fee6e8c30edea
dlr1516 <dario.lodirizzini@unipr.it>
2022-06-24 17:20:59 +0200

    fixed errors in common/tls_scalar_consensus.h

--- common/app/test_tls_scalar_consensus.cpp
+++ common/app/test_tls_scalar_consensus.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     rofl::ParamMap params;
     std::vector<float> valuesSrc, valuesDst, valuesDif;
     std::vector<float> ranges;
-    std::string filenameCfg;
+    std::string filenameCfg, filenamePlot;
     float translTrue, range, translEst;
     int valuesNum;
     std::random_device randDev;
@@ -37,26 +37,27 @@ int main(int argc, char** argv) {
     params.read(argc, argv);
     params.getParam<std::string>("cfg", filenameCfg, "");
     params.read(filenameCfg);
-    params.getParam<int>("valuesNum", valuesNum, 5);
+    params.getParam<int>("valuesNum", valuesNum, 6);
     params.getParam<float>("translTrue", translTrue, -1.8);
     params.getParam<float>("range", range, 0.4);
+    params.getParam<std::string>("plot", filenamePlot, "diff.plot");
 
     std::cout << "Params:\n";
     params.write(std::cout);
     std::cout << std::endl;
 
-    ROFL_VAR1(BOOST_VERSION);
+    // ROFL_VAR1(BOOST_VERSION);
 
     std::cout << "Creates perfectly matching source and destination point clouds\n";
     for (int i = 0; i < valuesNum; i++) {
-        valuesSrc.push_back(1.0 * i);
+        valuesSrc.push_back(0.8 * i * i);
         valuesDst.push_back(valuesSrc.back() + translTrue);
     }
     ROFL_VAR2(valuesSrc.size(), valuesDst.size());
 
     std::cout << "Add random points (outliers) to point clouds\n";
     std::uniform_real_distribution<float> distr(-translTrue, valuesNum + translTrue);
-    for (int i = 0; i < valuesNum; ++i) {
+    for (int i = 0; 2 * i < valuesNum && i < 0; ++i) {
         valuesSrc.push_back(distr(randEng));
         valuesDst.push_back(distr(randEng));
     }
@@ -71,10 +72,24 @@ int main(int argc, char** argv) {
     }
     ROFL_VAR2(valuesDif.size(), ranges.size());
 
+    std::sort(valuesDif.begin(), valuesDif.end());
+
+    std::ofstream filePlot(filenamePlot);
+    if (!filePlot) {
+        ROFL_ERR("Cannot open file \"" << filenamePlot << "\"");
+        return -1;
+    }
+    filePlot << "plot '-' w p pt 7 ps 0.6\n";
+    for (int i = 0; i < valuesDif.size(); ++i) {
+        filePlot << " " << valuesDif[i] << " 0.0\n";
+    }
+    filePlot << "e\n";
+
     std::cout << "Performs histogram computation\n";
     std::vector<bool> inliers;
-    rofl::estimateTLSEstimation(valuesDif.begin(), valuesDif.end(),
-                                ranges.begin(), ranges.end(), translEst, inliers);
+    // rofl::estimateTLSEstimation(valuesDif.begin(), valuesDif.end(),
+    //                             ranges.begin(), ranges.end(), translEst, inliers);
+    rofl::estimateTLSEstimation2(valuesDif, ranges, translEst, inliers);
 
     std::cout << "Estimated translation: " << translEst << std::endl;
 
